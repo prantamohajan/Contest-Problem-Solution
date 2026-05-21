5d1de49852745b1e83c2d58d2fe8c8eec1acd2f5
Pm Pranta <heyiampm@gmail.com>
2026-05-22 00:33:30 +0600

    S - Reach the Target

---
+++ S - Reach the Target/problem.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout <<(X-Y) << "\n";
+    }
+
+    return 0;
+}
