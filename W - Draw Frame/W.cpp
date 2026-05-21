a24b451beb6a765ba1b045ca0f8cfc527e000a2a
Pm Pranta <heyiampm@gmail.com>
2026-05-22 00:36:42 +0600

    W - Draw Frame

---
+++ W - Draw Frame/W.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int H, W;
+    cin >> H >> W;
+
+    for(int i = 1; i <= H; i++) {
+        for(int j = 1; j <= W; j++) {
+            if(i == 1 || i == H || j == 1 || j == W) {
+                cout << '#';
+            } else {
+                cout << '.';
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
