700ba3e1da93f052252294aa59a87ef74bf9b9c5
Pm Pranta <heyiampm@gmail.com>
2026-05-22 00:35:57 +0600

    V - 455

---
+++ V - 455/problem.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    if (A != B && B == C) {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
+
+    return 0;
+}
