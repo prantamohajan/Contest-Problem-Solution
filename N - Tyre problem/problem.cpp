7f6359ff947aa95531c35191e9ffe0ef8d282047
Pm Pranta <heyiampm@gmail.com>
2026-05-21 18:09:09 +0600

    N - Tyre problem

---
+++ N - Tyre problem/problem.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, M;
+        cin >> N >> M;
+
+        int total = (2 * N) + (4 * M);
+        cout << total << endl;
+    }
+
+    return 0;
+}
