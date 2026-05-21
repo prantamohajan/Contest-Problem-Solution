dcebb677a855a340403291b7d694e064a372c5a6
Pm Pranta <heyiampm@gmail.com>
2026-05-21 17:36:20 +0600

    E - Even and odd numbers

--- E - Even and odd numbers/problem.cpp
+++ E - Even and odd numbers/problem.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
+    
     if( (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) &&
         (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) )   
     {
