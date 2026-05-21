cf975848295629513569f35d972156f4497a777b
Pm Pranta <heyiampm@gmail.com>
2026-05-21 18:13:26 +0600

    R - Best of Two

---
+++ R - Best of Two/problem.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << max(X, Y) << "\n";
+    }
+
+    return 0;
+}
