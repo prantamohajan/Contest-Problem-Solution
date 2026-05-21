1d985af7454cf1c8d0223d6a4aa55eb22be4ecb0
Pm Pranta <heyiampm@gmail.com>
2026-05-21 18:09:39 +0600

    O - Maximum Submissions

---
+++ O - Maximum Submissions/problem.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int X;
+        cin >> X;
+        cout << 2 * X << "\n";
+    }
+    return 0;
+}
