998f63219676039d0cedc8c45ccb1a0f917326b6
Pm Pranta <heyiampm@gmail.com>
2026-05-23 23:21:01 +0600

    AC - Status Code

---
+++ AC - Status Code/AC.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int S;
+    cin >> S;
+
+    if (S >= 200 && S <= 299)
+        cout << "Success";
+    else
+        cout << "Failure";
+}
